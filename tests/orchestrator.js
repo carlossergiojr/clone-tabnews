df5ce329e5cf741d757229b5d942c092c2d7d5f4
Carlos lima <carlosslimajr@hotmail.com>
2025-03-30 11:37:40 -0300

    adjusting orchestrator

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -11,8 +11,10 @@ async function waitForAllServices() {
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
-      return responseBody;
+
+      if (response.status !== 200) {
+        throw new Error();
+      }
     }
   }
 }
