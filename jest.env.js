1ac9994afe5a7c8880ad4efaaa93c2501052f874
Carlos lima <carlosslimajr@hotmail.com>
2025-04-02 12:19:11 -0300

    Fix `prettier` style linting

--- jest.env.js
+++ jest.env.js
@@ -1,2 +1,2 @@
-process.env.NODE_ENV = 'development'
-require('dotenv').config({ path: '.env.development' });
+process.env.NODE_ENV = "development";
+require("dotenv").config({ path: ".env.development" });
